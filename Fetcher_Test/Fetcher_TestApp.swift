7b170bcfaed72e96370d781903d1c94754f80b36
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-09-18 13:44:27 +0500

    Initial Commit

---
+++ Fetcher_Test/Fetcher_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetcher_TestApp.swift
+//  Fetcher_Test
+//
+//  Created by Dmitry Yastrebov on 18.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Fetcher_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
